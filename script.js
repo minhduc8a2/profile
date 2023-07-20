7140d17d1b6b980e1c835fb0c49f8fb603bad451
Duke Lee <minhduc8a2.1@gmail.com>
2023-07-20 20:49:16 +0700

    big change

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function isInViewport(el) {
-  const rect = el.getBoundingClientRect();
-  const elementWidth = el.offsetWidth;
-  const elementHeight = el.offsetHeight;
+  const rect = el.getBoundingClientRect()
+  const elementWidth = el.offsetWidth
+  const elementHeight = el.offsetHeight
   return (
     rect.top + elementHeight >= 0 &&
     rect.left + elementWidth >= 0 &&
@@ -9,41 +9,39 @@ function isInViewport(el) {
       (window.innerHeight || document.documentElement.clientHeight) &&
     rect.right - elementWidth <=
       (window.innerWidth || document.documentElement.clientWidth)
-  );
+  )
 }
 setInterval(() => {
   const slideToLeftElements = document.querySelectorAll(
     ".ready-for-slide-to-left"
-  );
+  )
   slideToLeftElements.forEach((element) => {
-    if (isInViewport(element)) element.classList.add("slide-to-left");
-    else element.classList.remove("slide-to-left");
-  });
+    if (isInViewport(element)) element.classList.add("slide-to-left")
+  })
   const slideToRightElements = document.querySelectorAll(
     ".ready-for-slide-to-right"
-  );
+  )
   slideToRightElements.forEach((element) => {
-    if (isInViewport(element)) element.classList.add("slide-to-right");
-    else element.classList.remove("slide-to-right");
-  });
+    if (isInViewport(element)) element.classList.add("slide-to-right")
+  })
 
   //top
   const slideToTopElements = document.querySelectorAll(
     ".ready-for-slide-to-top"
-  );
+  )
   slideToTopElements.forEach((element) => {
-    if (isInViewport(element)) element.classList.add("slide-to-top");
-    else element.classList.remove("slide-to-top");
-  });
+    if (isInViewport(element)) element.classList.add("slide-to-top")
+  })
 
   //rotate
-  const rotateElements = document.querySelectorAll(".ready-for-rotate");
+  const rotateElements = document.querySelectorAll(".ready-for-rotate")
   rotateElements.forEach((element) => {
-    if (isInViewport(element)) element.classList.add("rotateAnimation");
-    else element.classList.remove("rotateAnimation");
-  });
-}, 100);
+    if (isInViewport(element)) element.classList.add("rotateAnimation")
+  })
 
+  // opacity all
+  
+}, 100)
 
 // const sendBtn = document.querySelector("#submit-btn");
 // sendBtn.addEventListener("click",(e)=>{
@@ -53,5 +51,5 @@ setInterval(() => {
 //   const inputEmail = document.querySelector('input[name="email"]');
 //   const inputMessage = document.querySelector('input[name="message"]');
 //   if(!inputName || !inputMessage || !inputEmail|| !inputSubject) return;
-//   location.replace(`mailto:minhduc8a2.1@gmail.com?subject=${inputSubject}&body=${inputMessage}`); 
+//   location.replace(`mailto:minhduc8a2.1@gmail.com?subject=${inputSubject}&body=${inputMessage}`);
 // })
